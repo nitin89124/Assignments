052c475d51b594b21d15ddcb4b1eb966ae755423
nitin89124 <133915161+nitin89124@users.noreply.github.com>
2023-05-17 15:59:24 -0400

    Add files via upload

---
+++ Py-05172023.py
@@ -0,0 +1,8 @@
+#### name = input("What is your name  ?  ")
+#### print(type(name), name)
+### print ("Hello World,\n\tMy Name is Nitin",end="---")
+#### print("Goodbye")
+### print("Goodbye","1233", "dddddd", sep='--',end='||||')
+
+name = "John"
+print(name)
